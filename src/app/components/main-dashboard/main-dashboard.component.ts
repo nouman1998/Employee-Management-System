61f48b3bf2e4109c4292d9f26a7fcd79d2250557
HassanFayyaz <hassanfayyaz440@gmail.com>
2020-06-22 16:08:16 -0700

    DashboardWorkDone

--- src/app/components/main-dashboard/main-dashboard.component.ts
+++ src/app/components/main-dashboard/main-dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { MainServiceService } from 'src/app/services/main-service.service';
 
 @Component({
   selector: 'app-main-dashboard',
@@ -7,9 +9,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainDashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router, private activateRoute: ActivatedRoute, private service: MainServiceService) { }
 
   ngOnInit(): void {
+    this.getTotalEmployeeCount();
+    this.getTotalDepartmentCount();
+    this.getTotalSalaryCodeCount();
+    this.getTotalJobCodeCount();
+    this.getTotalLeaveCodeCount();
+    this.getTotalSumOfSalaries();
+  }
+
+  goToListEmployee(){
+    this.router.navigate(['main/list-emp'])
+  }
+
+  goToListDepartment(){
+    this.router.navigate(['main/list-dept'])
+  }
+
+  goToListSalaryCode(){
+    this.router.navigate(['main/salary-list'])
+  }
+
+  goToListJobCode(){
+    this.router.navigate(['main/list-job'])
+  }
+  
+  goToListLeaveCode(){
+    this.router.navigate(['main/list-leaveCode'])
+  }
+
+  employeeCount
+  getTotalEmployeeCount(){
+    this.service.getTotalEmployeeCount().subscribe(d=>{
+      console.log("Count",d)
+      this.employeeCount = d.result;
+    })
+  }
+
+  departmentCount
+  getTotalDepartmentCount(){
+    this.service.getTotalDepartmentCount().subscribe(d=>{
+      this.departmentCount=d.result;
+    })
+  }
+
+  salaryCodeCount
+  getTotalSalaryCodeCount(){
+    this.service.getTotalSalaryCodeCount().subscribe(d=>{
+      this.salaryCodeCount=d.result;
+    })
+  }
+
+  jobCodeCount
+  getTotalJobCodeCount(){
+    this.service.getTotalJobCodeCount().subscribe(d=>{
+      this.jobCodeCount=d.result;
+    })
+  }
+
+  leaveCodeCount
+  getTotalLeaveCodeCount(){
+    this.service.getTotalLeaveCodeCount().subscribe(d=>{
+      this.leaveCodeCount=d.result;
+    })
+  }
+
+  totalSumOfSalaries
+  getTotalSumOfSalaries(){
+    this.service.getTotalSumOfSalaries().subscribe(d=>{
+      this.totalSumOfSalaries=d.result;
+    })
   }
 
 }
